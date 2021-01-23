fc112a51727fae94715604ab06c3e707cea85854
bilalbirolacar <75049474+bilalbirolacar@users.noreply.github.com>
2021-01-23 18:16:28 +0300

    Add files via upload

---
+++ styles.php
@@ -0,0 +1,590 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- basic page needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>KaraKaşKaraGöz</title>
+	<meta name="description" content="">  
+	<meta name="author" content="">
+
+   <!-- mobile specific metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+ 	<!-- CSS
+   ================================================== -->
+   <link rel="stylesheet" href="css/base.css">  
+   <link rel="stylesheet" href="css/main.css">
+   <link rel="stylesheet" href="css/vendor.css">
+
+   <style type="text/css" media="screen">
+
+   	#styles { 
+   		background: white;
+   		padding-top: 12rem;
+   		padding-bottom: 12rem;
+   	}
+   	#styles .row {
+   		max-width: 1024px;
+     	}
+     	#styles .section-Anasayfa {
+   		max-width: 800px;
+     	}
+      	
+   </style>       
+
+   <!-- script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+	<script src="js/pace.min.js"></script>
+
+</head>
+
+<body id="top">
+
+	<!-- header 
+   ================================================== -->
+   <header>   	
+	<div class="row">
+
+		<div class="top-bar">
+			<a class="menu-toggle" href="#"><span>Menu</span></a>
+
+			<div class="">
+			  <a href="index.html"></a>
+		   </div>		      
+
+			<nav id="main-nav-wrap">
+				 <ul class="main-navigation">
+					 <li class="current"><a class="smoothscroll"  href="#Anasayfa" title="">Anasayfa</a></li>
+					 <li><a class="smoothscroll"  href="#Hakkinda" title="">Hakkında</a></li>
+					 <li><a class="smoothscroll"  href="#Egitim" title="">Eğitim</a></li>
+					 <li><a class="smoothscroll"  href="#Sehir" title="">Şehir</a></li>
+					 <li><a class="smoothscroll"  href="#Hizmetler" title="">Hizmetler</a></li>					
+					 <li><a class="smoothscroll"  href="#contact" title="">İletişim</a></li>	
+					 <li><a href="styles.php" title="">Çalışma Alanı</a></li>				
+				 </ul>
+			 </nav>    		
+		</div> <!-- /top-bar --> 
+		
+	</div> <!-- /row --> 		
+</header> <!-- /header -->
+
+	<!-- Anasayfa section
+   ================================================== -->
+   <section id="Anasayfa">   
+
+	<div class="Anasayfa-overlay"></div>	
+
+	<div class="Anasayfa-content">
+		<div class="row">
+
+			<div class="col-twelve">
+
+				<h5>SELAM. DOSTUM!</h5>
+				<h1>İsmim Bilal Birol ACAR.</h1>
+
+				<p class="Anasayfa-position">
+					<span></span>
+				 <span>BİLGİSAYAR TEKNOLOJİLERİ VE SİSTEMLERİ</span> 
+				 <span></span> 
+				</p>
+
+				<a class="button stroke smoothscroll" href="#Hakkinda" title="">HAKKIMDA DAHA FAZLA</a>
+
+			</div>  
+			
+		</div>   		 		
+	</div> <!-- /Anasayfa-content --> 
+
+	<ul class="Anasayfa-social">        
+	  <li><a href="https://www.facebook.com/profile.php?id=100005638398126" target="_blank"><i class="fa fa-facebook"></i></a></li>
+	  <li><a href="#"><i class=""></i></a></li>
+	  <li><a href="https://twitter.com/BilalBirolAcar3" target="_blank"><i class="fa fa-twitter"></i></a></li>
+	  <li><a href="#"><i class=""></i></a></li>
+	  <li><a href="https://www.instagram.com/bilalbirolacar/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+   </ul> <!-- /Anasayfa-social -->      	
+
+</section> <!-- /Anasayfa -->
+
+
+   <!-- Çalışma Alanı
+   ================================================== -->
+   <section id="styles">
+
+   	<div class="row section-Anasayfa">
+
+   		<div class="col-twelve">
+
+   			<h1>Stil Rehberi</h1>
+
+   			<p class="lead">Ben artık okumayı yazmayı öğrendim.Öyle çok seviniyorum ki rüyama bile giriyor. Dün akşam bütün harfler rüyamda beni çok eğlendirdiler.Onları okuyabildiğim için çok mutluydular.R,A,F salıncak olmuşlardı.I ve N tahtaravalli olmuşlardı.O harfi havuz olmuş,U harfi ise gemi olmuştu.Sanki onları okuyup yazabildiğimiz için bizi ödüllendiriyorlardı.</p>   			
+
+   		</div>
+
+     	</div> <!-- /row -->
+
+     	<div class="row">
+
+     		<div class="col-six tab-full">
+
+		      <h3>Paragraf ve Resim</h3>
+
+		      <p><a href="#"><img width="120" height="120" class="pull-left" alt="sample-image" src="images/sample-image.jpg"></a>
+				Ev her zamankinden sesliydi. Sanki tüm elektrikli aletler çalışıyordu. Dışardan gelen gürültüler de cabasıydı. Bir tek insan sesi yoktu içerde. Bağıra bağıra şarkı söylemeye başladı. Susturmak istiyordu onları. bildiği tüm şarkıları söyledi. Sınırlıydı bilebildiği şarkı sayısı. Anlamsız sözcüklerle bağıra bağıra ezgiler düzüyordu. Ev susmuştu. Bir tek onun anlamsız sözcükleri çınlıyordu duvarlarda. Birden kendi sesini duydu ve sustu. Gidip tek tek evdeki tüm elektrikli aletleri çalıştırdı. Şimdi ev her zamankinden daha sessizdi.</p>
+
+		  		<p>
+					Kedi ve yavrularını, hemen hemen her gün, aynı saatlerde, o kapıyı çalarken görmeye alışmıştı. Kedi hep "tııııııs" diye sesler çıkarıyordu, yavrularına bir zarar vereceği sanısına kapılarak. Bu kediyle barışmanın yolunu bulmalıydı. Yine bir akşam kapıyı çaldığında, kedi bir arabanın altından başını gösterdi ve her zamanki sesi çıkardı: "tııııııs". Kapıyı çalmayı sürdürdü, kedi de tıslamayı. Kapı açılmadı. Uzun uzun çaldı ve açılmadı kapı. Bir daha ne o kapıyı çaldı, ne de kediyi gördü. Sorun kendiliğinden çözülmüştü. Hayat böyleydi.	
+ 			   </p>
+
+            <p>A <a href="#">link</a>,
+  			   <abbr title="this really isn't a very good description">abbreviation</abbr>,
+  			   <strong>strong text</strong>,
+            <em>em text</em>,
+  			   <del>deleted text</del>, and
+            <mark>this is a mark text.</mark>
+            <code>.code</code>
+          	</p>
+
+         </div>
+
+         <div class="col-six tab-full">
+
+            <h3>Bardak</h3>
+
+	         <p class="drop-cap">Bardak boştu. Boş muydu? Evet boştu. "Öyleyse doldurmak gerek." diye geçirdi içinden. Bardağın doldurulmasına karar verilmişti verilmesine de, neyle doldurulacaktı ki? Evet, bütün sorun buydu. Olmak ya da olmamak gibi bir şeydi bu. Hem bardağı dolduracak sıvının rengine de karar verilecekti. Bu renge bağlı olarak, içeceği sıvının tadı, ısısı ve yoğunluğu da önemliydi. Asit ya da alkol barındırmalı mıydı bu sıvı? Bardak hâlâ boştu. Boş kalması iyiydi. Sorular ve sorunlar azalıyordu böylece. "Azalıyor muydu?" diye yazdı, bir soru imi koydu ve kalemi bıraktı.
+		  		</p>
+
+		      <h3>Küçük baskı</h3>
+
+		      <p>Ağaç mıydı onlar?
+		  		<small>Evet. Ağaçların yeşili gecenin karanlığında bile seçiliyordu.</small>
+		  		</p>
+
+		   </div>	         
+
+		</div> <!-- /row -->
+
+		<div class="row">
+
+		  	<div class="col-six tab-full">
+
+		      <h3>Alıntı</h3>
+
+		      <aside class="pull-quote">
+				   <blockquote>
+					   <p>Cümlenin kavrulmuş kısımlarının ağzınıza uçtuğu paradisematik bir ülkedir. Tüm güçlü İşaretlemenin bile kontrolü yoktur Hakkinda kör metinler neredeyse ortografik bir yaşamdır Bir gün Lorem Ipsum adında küçük bir kör metin satırı, dilbilgisinin uzak dünyasına gitmeye karar verdi.</p>
+					</blockquote>
+				</aside>
+
+		   </div>
+
+		   <div class="col-six tab-full">
+
+		      <h3>Tavsiyeler</h3>
+
+		      <blockquote cite="http://where-i-got-my-info-from.com">
+				   <p>İşiniz hayatınızın büyük bir bölümünü dolduracak ve gerçekten tatmin olmanın tek yolu harika bir iş olduğuna inandığınız şeyi yapmaktır. Ve harika bir iş yapmanın tek yolu, yaptığınız işi sevmektir. Henüz bulamadıysanız, aramaya devam edin. Yerleşmeyin. Kalbin tüm meselelerinde olduğu gibi, onu bulduğunuzda bileceksiniz.
+		         </p>
+		
+					<cite>
+					   <a href="#">Steve Jobs</a>
+					</cite>
+			   </blockquote>
+
+		      <blockquote>
+		         <p>İyi tasarım, olabildiğince az tasarımdır.</p>
+		         <cite>Dieter Rams</cite>
+		      </blockquote>
+
+		   </div>
+
+		</div> <!-- /row -->
+
+		<div class="row half-bottom">
+
+		 	<div class="col-six tab-full">
+
+		      <h3>Örnek Listeler</h3>
+
+		      <ol>
+				   <li>İşte bir örnek.</li>
+				   <li>Sıralı bir listenin.</li>
+				   <li>Bir üst lüste öğesi.
+				   	<ul>
+				   		<li>bir</li>
+				   		<li>iki</li>
+				   		<li>üç</li>
+				   	</ul>
+				   </li>
+				   <li>Bir liste öğesi.</li>
+			   </ol>
+
+		      <ul class="disc">
+				   <li>İşte bir örnek.</li>
+				   <li>Sıralı bir listenin.</li>
+			   </ul>	
+
+			   <h3>Tanım Listeleri</h3>	            
+
+		      <h5>a)Çok Satırlı Tanımlar (default)</h5>
+
+		      <dl>
+				   <dt><strong>Bu bir terim.</strong></dt>
+				      <dd>Bu, her ikisi de içinde yaşayan terimin tanımıdır.<code>dl</code>.</dd>
+				   <dt><strong>Başka Bir Dönem</strong></dt>
+				      <dd>Ve o da bir tanım alıyor, bu satır</dd>
+				      <dd>Bu 2<sup>nd</sup>Tek bir terim için tanım. A <code>dt</code>Ardından birden fazla gelebilir.<code>dd</code>s.</dd>
+			   </dl>
+
+		   </div>
+
+		   <div class="col-six tab-full">		                  
+
+		      <h3>Headers</h3>
+
+		      <h1>H1 Başlık</h1>
+		      <h2>H2 Başlık</h2>
+		      <h3>H3 Başlık</h3>
+		      <h4>H4 Başlık</h4>
+		      <h5>H5 Başlık</h5>
+		      <h6>H6 Başlık</h6>
+
+		      <h3>Butonlar</h3>
+
+		    	<p>
+		     		<a class="button button-primary full-width" href="#">Birincil Buton</a>
+		     		<a class="button full-width" href="#">Varsayılan Butonu</a>
+		     		<a class="button stroke full-width" href="#">Kontur Butonu</a>
+		     	</p>
+
+		   </div>
+
+		</div> <!-- /row -->
+
+		<div class="row">
+
+		   <div class="col-twelve">
+
+		    	<h3>İstatistik Sekmeleri</h3>
+
+		      <ul class="stats-tabs">
+				   <li><a href="#">1,234 <em>Azize</em></a></li>
+				   <li><a href="#">567 <em>Harun</em></a></li>
+				   <li><a href="#">23,456 <em>Necati</em></a></li>
+				   <li><a href="#">3,456 <em>Kibariye</em></a></li>
+				   <li><a href="#">456 <em>Şehmuz</em></a></li>
+				   <li><a href="#">26 <em>Seyhan</em></a></li>
+				</ul>	      		
+
+		   </div>	      	
+
+		</div> <!-- /row -->
+
+		<div class="row half-bottom">
+
+      	<div class="col-six tab-full">
+
+      		<h3>
+				Duyarlı Resim</h3>
+
+      		<p><img src=""></p>
+
+      	</div>
+
+      	<div class="col-six tab-full">
+
+      		<h3>Duyarlı Video</h3>
+
+      		<div class="fluid-video-wrapper">
+                  <iframe src="http://player.vimeo.com/video/14592941?title=0&amp;byline=0&amp;portrait=0&amp;color=F64B39" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> 
+            </div>
+
+      	</div>
+      	
+      </div> <!-- /row -->
+
+      <div class="row add-bottom">
+
+      	<div class="col-twelve">
+
+      		<h3>Tablolar</h3>
+      		<p>Doğru biçimlendirilmiş tablo işaretlemesini kullandığınızdan emin olun. <code>&lt;thead&gt;</code> and <code>&lt;tbody&gt;</code> kodu işlerken <code>table</code>.</p>
+
+      		<div class="table-responsive">
+
+      			<table>
+					  <thead>
+					    <tr>
+					      <th>İsim</th>
+					      <th>Yaş</th>
+					      <th>Cinsiyet</th>
+					      <th>Yer</th>					    
+					    </tr>
+					  </thead>
+					  <tbody>
+					    <tr>
+					      <td>Serhat DİKMEN</td>
+					      <td>16</td>
+					      <td>Erkek</td>
+					      <td>Samsun</td>					    
+					    </tr>
+					    <tr>
+					      <td>Zeynep İrem TUNÇEL</td>
+					      <td>21</td>
+					      <td>Kadın</td>
+					      <td>Ankara</td>			    
+					    </tr>
+					  </tbody>
+					</table>
+
+      		</div>     		
+
+      	</div>
+      	
+      </div> <!--row -->
+
+      <div class="row">
+
+		   <div class="col-six tab-full">
+
+		      <h3>Form Stilleri</h3>
+
+		         <form>
+			        	<div>
+			         	<label for="sampleInput">E-postanız</label>
+			         	<input class="full-width" type="E-posta" placeholder="test@mail.com" id="sampleInput">
+			         </div>
+			         <div>
+			         	<label for="sampleRecipientInput">
+							İletişim kurma nedeni</label>
+			         	<div class="ss-custom-select">
+			         		<select class="full-width" id="sampleRecipientInput">
+				           		<option value="Option 1">Sorular</option>
+				           		<option value="Option 2">Bildiri</option>
+				           		<option value="Option 3">Diğerleri</option>
+				         	</select>
+			         	</div>			         	
+			         </div>
+			       
+			       	<label for="exampleİleti">İleti</label>
+			       	<textarea class="full-width" placeholder="İletiniz" id="exampleİleti"></textarea>
+
+			        	<label class="add-bottom">
+			           	<input type="checkbox">			            
+			           	<span class="label-text">
+							Bir kopyasını kendinize gönderin</span>
+			        	</label>
+			       
+			        	<input class="button-primary" type="GÖNDER" value="GÖNDER">
+
+			      </form>	            
+
+		      </div>
+
+		      <div class="col-six tab-full">
+
+		      		<h3>Beceri Çubukları</h3>
+
+		      		<ul class="skill-bars">
+						   <li>
+						   	<div class="progress percent90"><span>90%</span></div>
+						   	<strong>HTML5</strong>
+						   </li>
+						   <li>
+						   	<div class="progress percent85"><span>85%</span></div>
+						   	<strong>CSS3</strong>
+						   </li>
+						   <li>
+						   	<div class="progress percent70"><span>70%</span></div>
+						   	<strong>JQuery</strong>
+						   </li>
+						   <li>
+						   	<div class="progress percent95"><span>95%</span></div>
+						   	<strong>PHP</strong>
+						   </li>
+						   <li>
+
+						   	<div class="progress percent75"><span>75%</span></div>
+						   	<strong>Wordpress</strong>
+						   </li>
+		      
+						</ul> <!-- /skill-bars -->
+
+			        
+			        <h3>Uyarı Kutuları</h3> 
+			        
+			        <div class="alert-box ss-error hideit">
+				        <p>İleti Hatası. İleti'niz Buraya Gelecek.</p>
+				        <i class="fa fa-times close"></i>
+			        </div><!-- /error -->
+			        
+			        <div class="alert-box ss-success hideit">
+				        <p>Başarılı İleti. İleti'niz Buraya Gelecek.</p>
+				        <i class="fa fa-times close"></i>
+			        </div><!-- /success -->
+			        
+			        <div class="alert-box ss-info hideit">
+				         <p>Bilgi İleti. İleti'niz Buraya Gelecek.</p>
+				        <i class="fa fa-times close"></i>
+			        </div><!-- /info -->
+			        
+			        <div class="alert-box ss-notice hideit">
+				        <p>İleti'ye dikkat edin. İleti'niz Buraya Gelecek.</p>
+				        <i class="fa fa-times close"></i>
+			        </div><!-- /notice -->
+			        
+			  </div>	         
+
+		</div> <!-- /row -->
+		   	
+   </section> <!--styles -->	
+
+	
+   <!-- contact
+   ================================================== -->
+	<section id="contact">
+
+		<div class="row section-Anasayfa">
+   		<div class="col-twelve">
+
+   			<h5>İletişim</h5>
+   			<h1>İletişim Kurmaktan Çekinmeyin.</h1>
+
+   			<p class="lead">Lorem ipsum Do commodo in proident enim in dolor cupidatat adipisicing dolore officia nisi aliqua incididunt Ut veniam lorem ipsum Consectetur ut in in eu do.</p>
+
+   		</div> 
+   	</div> <!-- /section-Anasayfa -->
+
+   	<div class="row contact-form">
+
+   		<div class="col-twelve">
+
+            <!-- form -->
+            <form name="contactForm" id="contactForm" method="post" action="">
+      			<fieldset>
+
+                  <div class="form-field">
+ 						   <input name="contactName" type="text" id="contactName" placeholder="Name" value="" minlength="2" required="">
+                  </div>
+                  <div class="form-field">
+	      			   <input name="contactE-posta" type="E-posta" id="contactE-posta" placeholder="E-posta" value="" required="">
+	               </div>
+                  <div class="form-field">
+	     				   <input name="contactKonu" type="text" id="contactKonu" placeholder="Konu" value="">
+	               </div>                       
+                  <div class="form-field">
+	                 	<textarea name="contactİleti" id="contactİleti" placeholder="İleti" rows="10" cols="50" required=""></textarea>
+	               </div>                      
+                 <div class="form-field">
+                     <button class="GÖNDERform">Gönder</button>
+                     <div id="GÖNDER-loader">
+                        <div class="text-loader">Gönderiliyor...</div>                             
+       				      <div class="s-loader">
+								  	<div class="bounce1"></div>
+								  	<div class="bounce2"></div>
+								  	<div class="bounce3"></div>
+								</div>
+							</div>
+                  </div>
+
+      			</fieldset>
+      		</form> <!-- Form End -->
+
+            <!-- contact-warning -->
+            <div id="İleti-warning"></div>            
+            <!-- contact-success -->
+      		<div id="İleti-success">
+               <i class="fa fa-check"></i>İletiniz gönderildi, teşekkürler!<br>
+      		</div>
+
+         </div> <!-- /col-twelve -->
+   		
+   	</div> <!-- /contact-form -->
+
+   	<div class="row contact-info">
+
+   		<div class="col-four tab-full">
+
+   			<div class="icon">
+   				<i class="icon-pin"></i>
+   			</div>
+
+   			<h5>ADRES</h5>
+   			<p>Yeniçaydurt, 14030 Yeniçaydurt/Bolu Merkez/Bolu<br>
+            14400 TÜRKİYE
+            </p>
+   		</div>
+
+   		<div class="col-four tab-full collapse">
+
+   			<div class="icon">
+   				<i class="icon-mail"></i>
+   			</div>
+   			<h5>E-POSTA</h5>
+   			<p>bilalb.acar14@gmail.com<br></p>
+   		</div>
+
+   		<div class="col-four tab-full">
+
+   			<div class="icon">
+   				<i class="icon-phone"></i>
+   			</div>
+   			<h5>TELEFON</h5>
+   			<p>(+90) 531 320 08 53<br></p>
+   		</div>
+   		
+   	</div> <!-- /contact-info -->
+		
+	</section> <!-- /contact -->
+
+
+   <!-- footer
+   ================================================== -->
+
+   <footer>
+	<div class="row">
+
+		<div class="col-six tab-full pull-right social">
+
+			<ul class="footer-social">        
+			 <li><a href="https://www.facebook.com/profile.php?id=100005638398126" target="_blank"><i class="fa fa-facebook"></i></a></li>
+			 <li><a href="#"><i class=""></i></a></li>
+			 <li><a href="https://twitter.com/BilalBirolAcar3" target="_blank"><i class="fa fa-twitter"></i></a></li>
+			 <li><a href="#"><i class=""></i></a></li>
+			 <li><a href="https://www.instagram.com/bilalbirolacar/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+		  </ul> 
+			 
+	 </div>
+
+	 <div class="col-six tab-full">
+		 <div class="copyright">
+			   <span>© Copyright BİLAL BİROL ACAR 2021.</span> 	         	
+			</div>		                  
+		 </div>
+
+		 <div id="go-top">
+			<a class="smoothscroll" title="Back to Top" href="#top"><i class="fa fa-long-arrow-up"></i></a>
+		 </div>
+
+	 </div> <!-- /row -->     	
+</footer>  
+
+<div id="preloader"> 
+   <div id="loader"></div>
+</div> 
+
+
+   <!-- Java Script
+   ================================================== --> 
+   <script src="js/jquery-2.1.3.min.js"></script>
+   <script src="js/plugins.js"></script>
+   <script src="js/main.js"></script>
+
+</body>
+
+</html>
